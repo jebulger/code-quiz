3eb0d3aa8fe4e858c704c51addb297669bd8cafa
Jack <jebulger1003@gmail.com>
2023-04-13 16:09:21 -0400

    added comments explaining code, and finished readme.md

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # Code Quiz
 
 ## About
+While on my path to becoming a full-stack web developer, I wanted a way to start to prepare myself for the interview process. It is commonly requested during the process, for the coder being interviewed, to be tasked with some kind of coding assessment. Typically, this assessment is a combination of multiple choice questions/coding tasks. This application goes through a couple of foundational questions about coding and some of the languages, helping the user brush up on some basic coding facts. Throughout the process of building it, I became much more familiar with JavaScript and how it is typed. Since I had never before made an application with JavaScript.
 
 ## Table of Contents
-
+- [Installation](#installation)
+- [Usage](#usage)
+- [Deployed-Application](#deployed-application)
 ## Installation
-
+All that is required is a device with access to a web browser.
 ## Usage
+Opening up the application will bring you here:
+### ![](./assets/images/04-hw-full-size.png)
+
+Select the start button to begin, and the questions will be generated on the page:
+### ![](./assets/images/04-hw-questions.png)
+
+When the start button is pressed, the timer will begin, and you only have 75 seconds to answer all the questions.
+### ![](./assets/images/04-timer.png)
+
+Each time an answer is selected. You will be notified whether that answer choice was correct or incorrect. Each time you answer a question incorrectly, the timer will go down by 10 seconds.
+
+At the end, your score will be the time you have remainaing. Your score will be displayed and a text box below will appear asking for your initials.
+### ![](./assets/images/04-hw-enter-initials.png)
+
+Enter your initials and you will be taken to the high score page, where you will see your recent entry, and any other previously stored scores.
+
+You can press the <strong>GO BACK</strong> button to return to the start page. If you didn't like your score, or it is starting to get crowded, just press <strong>CLEAR HIGH SCORES</strong> and all previous score entries will be wiped.
+### ![](./assets/images/04-high-score-page.png)
+
+Clicking the text here will take you to the high score page at any point:
+### ![](./assets/images/04-hw-feature-high-score.png)
 
 ## Deployed Application
+**Live URL here: [_https://jebulger.github.io/code-quiz/_](https://jebulger.github.io/code-quiz/)**
+
+### ![](./assets/images/04-hw-full-size.png)
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Grabbing html elements and storing them into variables.
 var startButton = document.querySelector(".start-button");
 var timerText = document.querySelector(".timer");
 var startPage = document.getElementById("card-start");
@@ -8,20 +9,26 @@ var answerResult = document.querySelector(".result");
 var header = document.querySelector(".header");
 var finalScore = document.querySelector(".final-score");
 
+// Sets the start time for the timer
 var secondsLeft = 75;
 
+// This is selecting the h3 tag found within the section containing the questions
+// This h3 tag is what is updated to physically display the question itself
 var question = document.querySelector(".questions");
 
+// Query selectors for the input buttons representing the multiple choice answers
 var answerOne = document.querySelector(".answer1");
 var answerTwo = document.querySelector(".answer2");
 var answerThree = document.querySelector(".answer3");
 var answerFour = document.querySelector(".answer4");
 
+// Set to zero by default, this variable is used to track which input button the user selected
 var userChoice = 0;
-var correctAnswer;
 
+// Set to zero by default, is updated at the end of quiz to store seconds remaining as the user score
 var userScore = 0;
 
+// Array to store all the questions
 var questions = [
     "Commonly used data types DO Not include:",
     "The condition in an if/else statement is enclosed with:________.",
@@ -29,16 +36,20 @@ var questions = [
     "String values must be enclosed within ___________ when being assigned to variables.",
     "A very useful tool used during development and debugging for printing content to the debugger is:"
 ]
-
+// Array to store all the answers
 var answersQ1 = ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
 var answersQ2 = ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
 var answersQ3 = ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
 var answersQ4 = ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"]
 var answersQ5 = ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
 
+// Before any functions get ran, the questions is added a class of hidden
+// and the submit form is added a class of hidden
 hiddenQuestions.classList.add("hidden")
 submit.classList.add("hidden");
 
+// This starts the timer, and removes the hidden class from the questions and gives them a new class
+// for styling, also hides the start section and then the questions themselves are rendered
 function startGame() {
     startButton.disabled = true;
     startTimer()
@@ -48,6 +59,8 @@ function startGame() {
     renderQuestions()
 }
 
+// Function is for the timer, it will go down by 1 each second the function is ran
+// When it reaches zero, the timer clears itself  the quiz is over and the submit form is revealed
 function startTimer() {
     timerText.textContent = "Time: " + secondsLeft;
     var timerInterval = setInterval(function () {
@@ -61,7 +74,9 @@ function startTimer() {
             finalScore.textContent = "Your final score is: " + secondsLeft;
             hiddenQuestions.classList.remove("questions-revealed");
             hiddenQuestions.classList.add("hidden");
+            // Used to style the message letting the user know if they are wrong or correct
             answerResult.style.marginLeft = "15vw";
+            // This removes the result message after 2 and a half seconds of loading the submit form
             setTimeout(() => {
                 answerResult.style.display = "none";
             }
@@ -70,8 +85,15 @@ function startTimer() {
     }, 1000)
 }
 
+// By default starts the current question at 1;
 var currentQuestion = 1;
 
+// function for displaying the questions and answers themselves
+// checks to see what question is currently being displayed
+// If there are still more questions left it will update the current question
+// and move to the next one.
+// Since there are not more than 5 questions, after the 5th question is answered
+// The questions will be hidden, and the submit form is revealed
 function renderQuestions() {
         if (currentQuestion === 1) {
             question.textContent = questions[0];
@@ -123,6 +145,11 @@ function renderQuestions() {
         }
 }
 
+// Had to add one to the currentQuestion count when checking answer validity
+// due to the count increasing each time the questions are rendered.
+// But this function will check if the user selected a correct or wrong answer
+// and will let them know.
+// If an answer was wrong, 10 gets subtracted from the timer.
 function checkValidity() {
     if (currentQuestion == 2 && userChoice !== 3) {
         secondsLeft = Math.floor(secondsLeft - 10);
@@ -147,9 +174,9 @@ function checkValidity() {
     }
 }
 
-var userName = "";
-var score = "";
-
+// Function to run in the submitButton event listener
+// When ran, the function will prevent default, grab previous stored
+// high scores, and then push the new score into the array containing previous scores.
 function pressSubmit(event) {
     event.preventDefault();
     var userSubmission = document.querySelector(".user-submission").value;
@@ -162,6 +189,12 @@ function pressSubmit(event) {
     window.location.href="high-score-page.html";
 }
 
+// Each button choice is assigned a different value to keep track of which button is being clicked
+// When the checkValidity() is run, it will compare which question the page is currently on vs what
+// the user just selected
+// Regardless of the outcome the function to render questions gets ran again.
+// If there are no more questions to be rendered, the renderQuestions() will catch it,
+// and hhide the questions and display the submit form.
 buttonChoice1 = document.querySelector("#one").onclick = function() {
     userChoice = 1;
     checkValidity();
@@ -183,6 +216,9 @@ buttonChoice4 = document.querySelector("#four").onclick = function() {
     renderQuestions();
 }
 
+// When clicked the startGame function will run
 startButton.addEventListener("click", startGame);
 
+// When clicked the pressSubmit function will run, grabbing local storage data,
+// and storing new high score entry and pushing that entry into the local storage
 submitButton.addEventListener("click", pressSubmit);
